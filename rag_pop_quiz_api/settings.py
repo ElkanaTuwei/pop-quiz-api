3829eda25d823ecac1dcc2f4de75523204048137
elieltk <elkanatuwei4@gmail.com>
2020-04-06 03:23:21 +0300

    heroku config

--- rag_pop_quiz_api/settings.py
+++ rag_pop_quiz_api/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from django.core.exceptions import ImproperlyConfigured
-
+import django_heroku
+#activate heroku
+django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
